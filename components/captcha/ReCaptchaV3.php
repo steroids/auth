61213edce5221a0a8a3d30c4f4564bc316907d5c
Семён Зверев <drophere.box@gmail.com>
2020-09-28 14:06:48 +0700

    #36 add captcha validator

--- components/captcha/ReCaptchaV3.php
+++ components/captcha/ReCaptchaV3.php
@@ -1,14 +1,36 @@
 <?php
 
 
-namespace auth\components\captcha;
+namespace steroids\auth\components\captcha;
 
+
+use http\Client;
+use yii\db\Exception;
 
 class ReCaptchaV3 implements CaptchaComponentInterface
 {
+    public string $secretKey;
+
+    /**
+     * @param string $token
+     * @return bool
+     * @throws Exception
+     */
     public function validate(string $token): bool
     {
-        // @todo
-        return true;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify' . http_build_query([
+                'secret' => $this->secretKey,
+                'response' => $token,
+            ]));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        if (!isset($result['success'])) {
+            throw new Exception('Invalid recaptcha verify response.');
+        }
+
+        return (bool)$result['success'];
     }
 }
